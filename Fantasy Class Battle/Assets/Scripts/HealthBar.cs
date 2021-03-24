0281be52a82f84b715ba1ea60df182d9ff4ed92d
Patrick Dang <patrickdangt@gmail.com>
2021-03-24 03:09:27 -0700

    Player Template Creation

---
+++ Fantasy Class Battle/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text health_text;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+        health_text.text = health.ToString() + "/" + health.ToString();
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        health_text.text = health.ToString() + "/" + slider.maxValue;
+    }
+}
